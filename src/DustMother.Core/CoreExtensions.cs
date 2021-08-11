d439fb26785bbb736d9989fc4e7da35f03cfcc92
Alistair Chapman <alistair@agchapman.com>
2021-08-11 17:40:33 +1000

    Refactor DustMother.Core for netstandard20; Add extra properties to various save types; Add/update extension methods;

---
+++ src/DustMother.Core/CoreExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DustMother.Core
+{
+    internal static class CoreExtensions
+    {
+        internal static IEnumerable<T> SkipLast<T>(this IEnumerable<T> source, int n = 1)
+        {
+            var it = source.GetEnumerator();
+            bool hasRemainingItems = false;
+            var cache = new Queue<T>(n + 1);
+
+            do
+            {
+                if (hasRemainingItems = it.MoveNext())
+                {
+                    cache.Enqueue(it.Current);
+                    if (cache.Count > n)
+                        yield return cache.Dequeue();
+                }
+            } while (hasRemainingItems);
+        }
+    }
+}
---
+++ src/DustMother.Core/NameService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DustMother.Core
+{
+    public static class NameService
+    {
+        private static Dictionary<string, string> _specialNames = new Dictionary<string, string>
+        {
+            ["mig21_1"] = "T-21"
+        };
+        public static string GetAircraftName(string input, bool useDefault = true)
+        {
+            if (_specialNames.TryGetValue(input, out var specialName))
+            {
+                return specialName;
+            }
+            try
+            {
+                var nameParts = input.Split(new[] { '_' }, StringSplitOptions.RemoveEmptyEntries);
+                var codeParts = nameParts.Where(a => a.All(char.IsDigit)).ToList();
+                var trimmedParts = codeParts.Count > 1 ? codeParts.SkipLast(1) : new List<string>();
+                return string.Join("-", nameParts.Where(a => !a.All(char.IsDigit)).Concat(trimmedParts).Select(p => p.ToUpper()));
+            } catch
+            {
+                return input.ToUpper();
+            }
+            
+        }
+
+        public static string GetObjectiveType(string input, string defaultValue = null)
+        {
+            var knownNames = new Dictionary<string, string>
+            {
+                ["NewEnumerator1"] = "AA Suppression",
+                ["NewEnumerator4"] = "Fleet Hunt",
+                ["NewEnumerator5"] = "Transport Hunt",
+                ["NewEnumerator6"] = "Experimental Unit",
+                ["NewEnumerator7"] = "Most Wanted",
+                ["NewEnumerator9"] = "Score Attack"
+            };
+            var name = input.Split(new[] { ':' }, StringSplitOptions.RemoveEmptyEntries).Last();
+            if (knownNames.TryGetValue(name, out var objName))
+            {
+                return objName;
+            } else
+            {
+                return defaultValue ?? $"Unknown ({new string(name.Where(char.IsDigit).ToArray())}";
+            }
+        }
+    }
+}
